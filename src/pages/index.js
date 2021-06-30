ba2ffc14942492ea7750bda350c2394f0ec2a5a4
stuart-t-99 <79085043+stuart-t-99@users.noreply.github.com>
2021-06-30 15:41:40 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Key Azure ML training resources</h1>
+    <p>https://github.com/Azure/MachineLearningNotebooks/</p>
+    <p>https://github.com/MicrosoftLearning/ms-learn-dp100</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
@@ -19,11 +19,7 @@ const IndexPage = () => (
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
+      </Layout>
 )
 
 export default IndexPage
