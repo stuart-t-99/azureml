2a2ad4ea4a6e9284ca9cfd820a040bcbdfa1fc1f
stuart-t-99 <79085043+stuart-t-99@users.noreply.github.com>
2021-08-05 17:03:32 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,7 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Key learning resources</h1>
-    <a href="https://github.com/Azure/MachineLearningNotebooks/">https://github.com/Azure/MachineLearningNotebooks/</a>
-    <p> </p>
-    <a href="https://github.com/MicrosoftLearning/mslearn-dp100/">https://github.com/MicrosoftLearning/mslearn-dp100</a>  
-    <p></p>
-    <StaticImage
+  <StaticImage
       src="../images/azureml.png"
       width={150}
       quality={95}
@@ -21,6 +16,16 @@ const IndexPage = () => (
       alt="Azure ML"
       style={{ marginBottom: `1.45rem` }}
     />
+  
+  
+  <h1>Key learning resources</h1>
+    <h2>Starting with AzureML</h2>
+    <a href="https://github.com/Azure/MachineLearningNotebooks/">https://github.com/Azure/MachineLearningNotebooks</a><p> </p>
+    <a href="https://github.com/MicrosoftLearning/mslearn-dp100/">https://github.com/MicrosoftLearning/mslearn-dp100</a><p></p>
+    <h2>MLOps examplesL</h2>
+    <a href="https://github.com/microsoft/MLOps/">https://github.com/microsoft/MLOps</a><p></p>
+  
+  
       </Layout>
 )
 
