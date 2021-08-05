fdee71c80e149552b92998180ef6718b06ef30e9
stuart-t-99 <79085043+stuart-t-99@users.noreply.github.com>
2021-08-05 17:10:35 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <Seo title="Home" />
   <StaticImage
       src="../images/azureml.png"
-      width={150}
+      width={75}
       quality={95}
       formats={["AUTO", "WEBP", "AVIF"]}
       alt="Azure ML"
@@ -20,9 +20,10 @@ const IndexPage = () => (
   
   <h1>Key learning resources</h1>
     <h2>Starting with AzureML</h2>
-    <a href="https://github.com/Azure/MachineLearningNotebooks/">https://github.com/Azure/MachineLearningNotebooks</a><p> </p>
-    <a href="https://github.com/MicrosoftLearning/mslearn-dp100/">https://github.com/MicrosoftLearning/mslearn-dp100</a><p></p>
-    <h2>MLOps examplesL</h2>
+    <a href="https://github.com/Azure/MachineLearningNotebooks/">ttps://github.com/Azure/MachineLearningNotebooks</a><p> </p>
+    <a href="https://github.com/MicrosoftLearning/mslearn-dp100/">ttps://github.com/MicrosoftLearning/mslearn-dp100</a><p></p>
+    <a href="https://azure.github.io/azureml-cheatsheets/">s://azure.github.io/azureml-cheatsheets/</a><p></p>
+    <h2>MLOps examples</h2>
     <a href="https://github.com/microsoft/MLOps/">https://github.com/microsoft/MLOps</a><p></p>
   
   
